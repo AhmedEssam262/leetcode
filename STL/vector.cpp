1c774a4e985480793bf2080692fdb9b1083fa86c
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-08-27 19:03:03 +0200

    Create vector.cpp

---
+++ STL/vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	vector<int> v1 = { 1,2,3,4,5 };
+  // Vector constructor
+	vector<int> v(v1);
+  vector<int> v(5,100);                   // a vector with size=5 and value=100 
+
+
+
+}
