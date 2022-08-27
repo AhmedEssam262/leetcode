903ef0d546d52d327e1fd81926700caddd68698d
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-08-27 20:41:12 +0200

    Create deque.cpp

---
+++ STL/deque.cpp
@@ -0,0 +1,13 @@
+// The best important advantage is that it is faster toinsert from the both sides
+
+#include<iostream>
+#include<deque>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	deque<char> d = {'h','m','e'};
+	d.push_back('d');
+	d.push_front('a');
+}
