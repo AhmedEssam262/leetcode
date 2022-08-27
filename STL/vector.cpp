ac9cb736ffdab9d4a7484d6302c437209ce7e961
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-08-27 19:39:00 +0200

    Update vector.cpp

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -8,7 +8,31 @@ int main() {
   // Vector constructor
 	vector<int> v(v1);
   vector<int> v(5,100);                   // a vector with size=5 and value=100 
+/*
+--------------------------------------------------------------------------Iterator--------------------------------------------------------------------------
+*/
+	
+vector<int>::reverse_iterator it;
+	it = v.rbegin();
+	for (; it != v.rend(); it++) {
+		cout << *it << endl;
+	}	
+	vector<int>::iterator it;
+	it = v.begin();
 
-
+	for (; it != v.end(); it++) {
+		cout << *it << endl;
+	}
+	// there is also crbegin so we will use auto
+	auto it=v.rbegin();
+/*
+--------------------------------------------------------------------------functions--------------------------------------------------------------------------
+*/
+// erase.insert : take pointer
+	v.insert(v.begin()+3, 100);
+	v.erase(v.begin()+3);
+//resize
+	v.resize(4);
+	v.shrink_to_fit();
 
 }
