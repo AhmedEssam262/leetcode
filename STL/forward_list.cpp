9729eb6018bee30df04838bff662245151ff1f18
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-08-28 13:12:16 +0200

    Create forward_list.cpp

---
+++ STL/forward_list.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+#include<forward_list>
+using namespace std;
+
+bool rem(int x) {
+	return x % 2 == 0;
+}
+int main() {
+	forward_list<int> l = { 1,3,4,5,6 };
+	auto it = l.before_begin();
+	
+	l.insert_after(it, { 10,20,30,40 });
+	//l.erase_after(it);
+
+	for (int i : l) {
+		cout << i << endl;
+	}
+  
+  
+  
+  forward_list<int> m = { 1,3,4,5,6 };
+	forward_list<int> n = { 10,30,40,50,60 };
+	auto it = m.before_begin();
+	
+	n.splice_after(n.before_begin(), m);
+
+	for (int i : n) {
+		cout << i << endl;
+	}
+}
