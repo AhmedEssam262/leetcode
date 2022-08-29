73b02648b5ba0ea7bf92b7c02874eb517e0d791a
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-08-29 16:25:54 +0200

    Update set.cpp

--- STL/set.cpp
+++ STL/set.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 int main() {
+
 	set<int> s = {20,30,10,5,15,45,25,60,50};
 	auto s_it = s.find(10);
+	//auto s_it = s.lower_bound(10);
 	auto e_it = s.find(25);
+	//auto e_it = s.upper_bound(25);
 	s.erase(s_it, e_it);
 
 	for (auto i : s) {
 		cout << i << endl;
+	}
+	cout << "----------------------------------------------------------------------" << endl;
+	int c = 30;
+	auto com = s.key_comp();
+	for (auto itt = s.begin(); com(*itt, c); itt++) {
+		cout << *itt << endl;
 	}
 }
