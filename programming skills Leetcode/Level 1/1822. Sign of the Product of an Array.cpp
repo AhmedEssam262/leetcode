73594c71d5f07b5d12cb9a28eddcc99484dc94f0
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2023-03-06 07:47:15 +0200

    Create 1822. Sign of the Product of an Array.cpp

---
+++ programming skills Leetcode/Level 1/1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int s=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                return 0;
+            else if(nums[i] <0 )
+                s++;
+        }
+        
+        if(s%2==0)
+            return 1;
+        else
+            return -1;
+    }
+};
