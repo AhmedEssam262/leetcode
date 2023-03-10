7dc8d0768f7cf08aa7cbf762ec19438e5a6d712d
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2023-03-10 17:17:31 +0200

    Create 389. Find the Difference.cpp

---
+++ programming skills Leetcode/Level 1/389. Find the Difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum=0;
+        for(int i=0; i<t.size(); i++){
+        sum+=t[i]-s[i];
+        }
+        return sum;
+}
+
+};
