f8a844c7ec7543b06d9f175d7072b45dae04bf53
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2023-03-12 06:01:43 +0200

    Create 1309. Decrypt String from Alphabet to Integer Mapping.cpp

---
+++ programming skills Leetcode/Level 1/1309. Decrypt String from Alphabet to Integer Mapping.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string freqAlphabets(string s) {
+
+
+	map<char, char> m1 = { {'1','a'},{'2','b'}, {'3','c'}, {'4','d'}, {'5','e'}, {'6','f'}, {'7','g'},
+					   {'8','h'}, {'9','i'} };
+	map<string, char> m2 = { {"10",'j'},{"11",'k'}, {"12",'l'}, {"13",'m'}, {"14",'n'}, {"15",'o'}, {"16",'p'},
+					   {"17",'q'}, {"18",'r'}, {"19",'s'}, {"20",'t'},
+					   {"21",'u'}, {"22",'v'}, {"23",'w'}, {"24",'x'},
+					   {"25",'y'}, {"26",'z'} };
+	string output = "";
+	for (int i = s.size() - 1; i >= 0; i--) {
+		if (s[i] == '#') {
+			string temp = "";
+			temp += s[i - 2];
+			temp += s[i - 1];
+			output = (m2[temp] + output);
+			i--;
+			i--;
+		}
+		else {
+			output = m1[s[i]] + output;
+		}
+	}
+	return output;
+}
+};
